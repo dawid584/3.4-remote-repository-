e6e7e37bba2a0e1139382e8013a094d8254ff2c7
Dawid Dymke <dawid584@yahoo.com>
2022-07-25 16:33:00 +0200

    Init project

--- samplefile.py
+++ samplefile.py
@@ -4,3 +4,9 @@ print("Lista zakupów:")
 amount = 0
 for shop_0 , product in shop.items():
     amount = amount + len(product)
+    
+    sp = [  x.capitalize() for x in product ]
+     
+    print(f'Idę do {shop_0.title()}, kupuję tu tam : {sp} ' )
+
+print(f"W sumie kupuję {amount} produktów.")
