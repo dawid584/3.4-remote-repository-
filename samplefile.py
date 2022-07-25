8dbf10a6b7a95ba52001508f7d8a0cf08ab00fab
Dawid Dymke <dawid584@yahoo.com>
2022-07-25 16:29:19 +0200

    Init project

--- samplefile.py
+++ samplefile.py
@@ -1,2 +1,6 @@
 sp = []
 shop = {"piekarnia" : ["chleb", "pączek", "bułki"] , "warzywniak" : ["marchew", "seler", "rukola"] }
+print("Lista zakupów:")
+amount = 0
+for shop_0 , product in shop.items():
+    amount = amount + len(product)
