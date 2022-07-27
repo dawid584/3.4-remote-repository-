0647addc6e4380bcfe3a890782f5d537c170671c
Dawid Dymke <dawid584@yahoo.com>
2022-07-27 14:08:55 +0200

    Init project 1

--- samplefile.py
+++ samplefile.py
@@ -1,12 +1,2 @@
 sp = []
 shop = {"piekarnia" : ["chleb", "pączek", "bułki"] , "warzywniak" : ["marchew", "seler", "rukola"] }
-print("Lista zakupów:")
-amount = 0
-for shop_0 , product in shop.items():
-    amount = amount + len(product)
-    
-    sp = [  x.capitalize() for x in product ]
-     
-    print(f'Idę do {shop_0.title()}, kupuję tu tam : {sp} ' )
-
-print(f"W sumie kupuję {amount} produktów.")
